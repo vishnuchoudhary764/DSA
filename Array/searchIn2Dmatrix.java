14150fd24910e2666b8da7699c38429f99875473
vishnuchoudhary764 <vasanaram162@gmail.com>
2026-06-22 08:04:24 +0530

    searchIn2DMatrix

---
+++ Array/searchIn2Dmatrix.java
@@ -0,0 +1,43 @@
+
+public class searchIn2Dmatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            int value = matrix[row][col];
+
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        searchIn2Dmatrix searcher = new searchIn2Dmatrix();
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+        int target = 3;
+        boolean result = searcher.searchMatrix(matrix, target);
+        System.out.println("Target " + target + " found: " + result);
+
+    }
+}
